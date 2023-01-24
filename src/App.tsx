b3fb12389777a9dcc5eaaec2e2dc1640e5b48a96
AIllarionov <hellomyguest@yandex.ru>
2023-01-24 14:09:32 +0300

    add contract

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,151 @@ export const App = () => {
       address: accounts[0],
       balance,
     };
-    console.log(user)
+    console.log(user);
     signIn(user);
+  };
+
+  const handleClickContractRead = async () => {
+    const web3 = new Web3(Web3.givenProvider);
+    const contract = new web3.eth.Contract(
+      [
+        { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+        {
+          inputs: [
+            { internalType: "address", name: "_address", type: "address" },
+          ],
+          name: "getCountGame",
+          outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            { internalType: "uint256", name: "_count", type: "uint256" },
+            { internalType: "address", name: "_address", type: "address" },
+          ],
+          name: "getLastSpin",
+          outputs: [
+            { internalType: "uint256[9][]", name: "", type: "uint256[9][]" },
+            { internalType: "uint256[]", name: "", type: "uint256[]" },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            { internalType: "uint256", name: "_count", type: "uint256" },
+          ],
+          name: "getLastWinSpin",
+          outputs: [
+            { internalType: "address[]", name: "", type: "address[]" },
+            { internalType: "uint256[9][]", name: "", type: "uint256[9][]" },
+            { internalType: "uint256[]", name: "", type: "uint256[]" },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "referralProgramContract",
+          outputs: [{ internalType: "address", name: "", type: "address" }],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "referralsContract",
+          outputs: [{ internalType: "address", name: "", type: "address" }],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "_address_Lottery",
+              type: "address",
+            },
+            {
+              internalType: "address",
+              name: "_address_RefferalProgram",
+              type: "address",
+            },
+            {
+              internalType: "address",
+              name: "_address_Referrals",
+              type: "address",
+            },
+          ],
+          name: "setContracts",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            { internalType: "uint256", name: "_price", type: "uint256" },
+          ],
+          name: "setMinPrice",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            { internalType: "address", name: "_address", type: "address" },
+          ],
+          name: "setReferralProgramContract",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            { internalType: "address", name: "_address", type: "address" },
+          ],
+          name: "setReferralsContract",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            { internalType: "address", name: "_address", type: "address" },
+          ],
+          name: "setWeeklyLotteryContract",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [{ internalType: "address", name: "_ref", type: "address" }],
+          name: "startGame",
+          outputs: [],
+          stateMutability: "payable",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "startGame",
+          outputs: [],
+          stateMutability: "payable",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "weeklyLotteryContract",
+          outputs: [{ internalType: "address", name: "", type: "address" }],
+          stateMutability: "view",
+          type: "function",
+        },
+      ],
+      "0x8014704FcB1BE803072981CB5a8f3504eb751d80"
+    );
+    const count = await contract.methods
+      .getCountGame(session?.address)
+      .estimateGas();
+    console.log('etimateGas ',count);
   };
 
   const handleClick = () => {
@@ -37,6 +180,9 @@ export const App = () => {
         </Button>
         <Button type="primary" onClick={handleClick}>
           show me the store
+        </Button>
+        <Button type="primary" onClick={handleClickContractRead}>
+          Contract
         </Button>
       </Header>
       <Content style={{ padding: "50px", height: "100%" }}>
